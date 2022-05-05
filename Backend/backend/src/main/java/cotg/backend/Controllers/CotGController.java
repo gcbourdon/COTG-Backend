1bb9dee53059f9f3f44268a9781701e5156e9f13
Griffin Bourdon <gcbourdon@gmail.com>
2022-05-04 17:12:19 -0700

    Initial commit of CotG Backend application

---
+++ Backend/backend/src/main/java/cotg/backend/Controllers/CotGController.java
@@ -0,0 +1,35 @@
+package cotg.backend.Controllers;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CotGController {
+
+    @GetMapping(path = "/")
+    public String hello() {
+        return "Hello World from CotG!";
+    }
+
+    @GetMapping(value = "/hello/{name}")
+    public String hello(@PathVariable String name) {
+
+        return String.format("Hello %s!", name);
+    }
+}
